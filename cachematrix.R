7ea5fccd544193842c594e3a52025193530f9be3
joeykavanagh <joeykavanagh@gmail.com>
2014-08-24 23:51:03 +0100

    Final Version of Assignment 2
    Includes better comments

--- cachematrix.R
+++ cachematrix.R
@@ -7,13 +7,19 @@
 
 makeCacheMatrix <- function(x = matrix()) {
         i <- NULL
+        ## set the value of inverse matrix 'i' to NULL as default
         set <- function(y) {
                 x <<- y
                 i <<- NULL
         }
+        ##function 'set' will restore inverse value to NULL if value
+        ## of matrix x is changed
         get <- function() x
+        ##function 'get' returns the value of x
         setinverse <- function(solve) i <<- solve
+        ##setinverse allows you to assign a value to inverse matrix 'i'
         getinverse <- function() i
+        ##function 'getinverse' returns the value of inverse matrix 'i'
         list(set = set, get = get,
              setinverse = setinverse,
              getinverse = getinverse)
@@ -26,17 +32,20 @@ makeCacheMatrix <- function(x = matrix()) {
 ## been calculated (and the matrix has not changed), then 
 ## cacheSolve should retrieve the inverse from the cache.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-        
+cachesolve <- function(x, ...) {
         i <- x$getinverse()
+        ##We set the value of i as the inverse of x, if this has 
+        ##been cached previously
                 if(!is.null(i)) {
                         message("getting cached data")
                         return(i)
                 }
+        ##If i is not a null matrix, we return its value
+        ##Otherwise we use the function solve to calculate i, the
+        ##inverse of x and return this value
                 data <- x$get()
                 i <- solve(data, ...)
                 x$setinverse(i)
                 i
-        }
 }
+
